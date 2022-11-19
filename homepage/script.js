36e021faebf741bf38e2ade1a5cb1bfb627631d3
christovanqueiroz <106829027+christovanqueiroz@users.noreply.github.com>
2022-11-19 17:29:01 -0300

    Add files via upload

---
+++ homepage/script.js
@@ -0,0 +1,15 @@
+const products = document.querySelectorAll('.product')
+
+products.forEach(product => {
+    product.addEventListener('mouseover', () => {
+        (product.querySelector('.addToCart')).style.visibility = 'visible';
+        (product.querySelector('.blur').style.filter = 'blur(2px')
+    })
+})
+
+products.forEach(product => {
+    product.addEventListener('mouseout', () => {
+        (product.querySelector('.addToCart')).style.visibility = 'hidden';
+        (product.querySelector('.blur').style.filter = 'none')
+    })
+})
