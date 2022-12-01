d347868f3bbffaaf7e520ac9945a1d5a56ec19bd
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2022-12-01 13:06:21 -0300

    new file:   homepage/assets/main/1.jpg 	new file:   homepage/assets/main/2.jpg 	new file:   homepage/assets/main/3.jpg 	new file:   homepage/assets/main/4.jpg 	new file:   homepage/assets/main/5.jpg 	new file:   homepage/assets/main/6.jpg 	deleted:    homepage/assets/main/Product1.png 	deleted:    homepage/assets/main/Product2.png 	deleted:    homepage/assets/main/Product3.png 	deleted:    homepage/assets/main/Product4.png 	deleted:    homepage/assets/main/Product5.png 	deleted:    homepage/assets/main/Product6.png 	modified:   homepage/homepage.html 	modified:   homepage/script2.js 	modified:   homepage/style.css

--- homepage/script2.js
+++ homepage/script2.js
@@ -1,24 +1,28 @@
 const addToCart = document.querySelectorAll('.addToCart');
+const openModal = document.querySelector('.loginPopup');
+const modal = document.querySelector('.loginModal');
+const body = document.querySelector('body');
+const modalBackContent = document.querySelector('.fade');
+const closeModal = document.querySelector('.btnCloseModal');
+let cartQty = document.querySelector('.cartQty p b');
 
 addToCart.forEach(card => {
     card.addEventListener('click', () => {
         (document.querySelector('.cartQty')).style.display = 'flex';
-        (document.querySelector('.cartQty')).innerText = (+1);
     })
 })
 
-const openModal = document.querySelector('.loginPopup')
-const modal = document.querySelector('.loginModal')
-const body = document.querySelector('body')
-const modalBackContent = document.querySelector('.fade');
+function sumFunction() {
+    cartQty.innerHTML = parseInt(cartQty.innerHTML) + 1
+}
+
+console.log(cartQty.innerHTML)
 
 openModal.addEventListener('click', function(){
     modal.classList.replace("loginModal", "loginModalVisible");
     body.style.overflow = "hidden"
     modalBackContent.style.display = "flex";
 })
-
-const closeModal = document.querySelector('.btnCloseModal')
 
 closeModal.addEventListener('click', function(){
     modal.classList.replace("loginModalVisible", "loginModal");
