0f3b762a2bee423f768c913f3147b687c9ba8028
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2022-12-27 16:59:19 -0300

    modified:   homepage/homepage.html 	modified:   homepage/script.js 	deleted:    homepage/script2.js 	modified:   homepage/style.css

--- homepage/script.js
+++ homepage/script.js
@@ -1,15 +1,62 @@
+const addToCart = document.querySelectorAll('.addToCart');
+let cartQty = document.querySelector('.cartQty span');
+
 const products = document.querySelectorAll('.product')
+
+const openLoginModal = document.querySelector('.loginPopup');
+const closeLoginModal = document.querySelector('.btnCloseLoginModal');
+const loginModal = document.querySelector('.loginModal');
+
+const openSizeChartModal = document.querySelector('.sizeChartPopup');
+const closeSizeChartModal = document.querySelector('.btnCloseSizeChartModal');
+const sizeChartModal = document.querySelector('.sizeChartModal');
+
+const modalFadeBackContent = document.querySelector('.fade');
+const body = document.querySelector('body');
+
+addToCart.forEach(card => {
+    card.addEventListener('click', () => {
+        (document.querySelector('.cartQty')).style.display = 'flex';
+    })
+})
+
+function sumFunction() {
+    cartQty.innerHTML = parseInt(cartQty.innerHTML) + 1
+}
+
+openLoginModal.addEventListener('click', function(){
+    loginModal.classList.replace("loginModal", "loginModalVisible");
+    body.style.overflow = "hidden";
+    modalFadeBackContent.style.display = "flex";
+    sizeChartModal.classList.replace("sizeChartModalVisible", "sizeChartModal")
+})
+
+closeLoginModal.addEventListener('click', function(){
+    loginModal.classList.replace("loginModalVisible", "loginModal");
+    body.style.overflow = "visible";
+    modalFadeBackContent.style.display = "none";
+})
+
+openSizeChartModal.addEventListener('click', function() {
+    sizeChartModal.classList.replace("sizeChartModal", "sizeChartModalVisible");
+})
+
+closeSizeChartModal.addEventListener('click', function(){
+    sizeChartModal.classList.replace("sizeChartModalVisible", "sizeChartModal");
+})
 
 products.forEach(product => {
     product.addEventListener('mouseover', () => {
         (product.querySelector('.addToCart')).style.visibility = 'visible';
         (product.querySelector('.blur').style.filter = 'blur(1.5px');
+        (product.querySelector('.sliders').style.visibility = 'visible');
     })
 })
 
 products.forEach(product => {
     product.addEventListener('mouseout', () => {
         (product.querySelector('.addToCart')).style.visibility = 'hidden';
-        (product.querySelector('.blur').style.filter = 'none')
+        (product.querySelector('.blur')).style.filter = 'none';
+        (product.querySelector('.sliders').style.visibility = 'hidden');
     })
 })
